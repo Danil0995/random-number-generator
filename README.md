f88ff4a4e6b97ff9a04956a560511398f81ed91d
Danilo <bonodanilo@outlook.it>
2025-04-25 18:01:09 +0200

    feat: backend project working

--- README.md
+++ README.md
@@ -1,3 +1,85 @@
 # RANDOM NUMBER GENERATOR
 
 This is a simple random stack number generator program developer by danilo during the lacerba lesson
+
+#install
+'''bash
+npm install
+
+#use backend
+'''bash
+ts-node main.ts
+
+example output:
+{
+  "Milano": [
+    70,
+    94,
+    5,
+    38,
+    95
+  ],
+  "Roma": [
+    1,
+    62,
+    91,
+    19,
+    70
+  ],
+  "Napoli": [
+    12,
+    92,
+    52,
+    73,
+    22
+  ],
+  "Torino": [
+    83,
+    11,
+    29,
+    47,
+    94
+  ],
+  "Firenze": [
+    58,
+    37,
+    26,
+    59,
+    19
+  ],
+  "Bologna": [
+    91,
+    31,
+    67,
+    71,
+    89
+  ],
+  "Venezia": [
+    56,
+    63,
+    9,
+    36,
+    94
+  ],
+  "Genova": [
+    80,
+    12,
+    36,
+    40,
+    26
+  ],
+  "Palermo": [
+    42,
+    19,
+    51,
+    84,
+    97
+  ],
+  "Catania": [
+    5,
+    11,
+    60,
+    96,
+    90
+  ]
+}
--- main.ts
+++ main.ts
@@ -1,1 +1,34 @@
-console.log("Hello, world!");
+function RNG(min: number, max: number) {
+    const rng = Math.random();
+    return Math.trunc(rng * (max - min + 1)) + min;
+}
+
+function RNGSequence(length: number, min: number, max: number) {
+    if (length > max - min) { 
+        throw new Error("Length is greater than the range of numbers.");
+    }
+    const res: number[] = [];
+    while (res.length < length) {
+        const rn = RNG(min, max);
+        if (res.includes(rn)) {
+            continue;
+        }
+        res.push(rn);
+    }
+    return res;
+}
+
+const route = ["Milano", "Roma", "Napoli", "Torino", "Firenze", "Bologna", "Venezia", "Genova", "Palermo", "Catania"];
+
+const estrazioni: { [ruota: string]: number[]} = {};
+
+for (const ruota of route) {
+const estrazione = RNGSequence(5, 1, 100);
+estrazioni [ruota] = estrazione;
+}
+
+console.log(JSON.stringify(estrazioni, null, 2));
+// function RNGdec(min: number, max: number, precision: number) {
+//     const multFactor = Math.pow(10, precision);
+//     return RNG(min * multFactor, max * multFactor) / multFactor;
+// }
